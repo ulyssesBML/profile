3afb16f561a715acd22c0f319f584703c459d0ae
ulyssesBML <ulysses3353@gmail.com>
2020-08-14 14:52:04 -0300

    config ghpages

---
+++ build/precache-manifest.a4395ee344d370b399de8b3d49a58561.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b3c10882ce1b827100bd68ec4311a5",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7837c8b43d6c2766abd8",
+    "url": "/profile/static/css/main.9e98b0b7.chunk.css"
+  },
+  {
+    "revision": "134923be7c68f07ca84a",
+    "url": "/profile/static/js/2.b52534ff.chunk.js"
+  },
+  {
+    "revision": "7837c8b43d6c2766abd8",
+    "url": "/profile/static/js/main.15025132.chunk.js"
+  },
+  {
+    "revision": "cb1f730ff8b5ef5583d9",
+    "url": "/profile/static/js/runtime-main.2717ce2c.js"
+  },
+  {
+    "revision": "6de09c408a79d51802df0b3d478737aa",
+    "url": "/profile/static/media/UlyssesLara_CV.6de09c40.pdf"
+  },
+  {
+    "revision": "e1f4c2515e03895dc23928dd52d60a4a",
+    "url": "/profile/static/media/p1.e1f4c251.png"
+  },
+  {
+    "revision": "d0ea323dd1dfce9170f6f03748e387bd",
+    "url": "/profile/static/media/p2.d0ea323d.png"
+  },
+  {
+    "revision": "365e44c94ec7dd2bc8d15dc80d8df41e",
+    "url": "/profile/static/media/p3.365e44c9.png"
+  },
+  {
+    "revision": "d4436d263dd1ae46bc1ad056b29b46e2",
+    "url": "/profile/static/media/p4.d4436d26.png"
+  }
+]);
